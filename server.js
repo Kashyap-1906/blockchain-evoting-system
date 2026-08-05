bdfb062a1ca889baebc6d32617e36b82acb3d63c
Kashyap-1906 <kashyapivaturi1906@gmail.com>
2026-08-05 15:42:47 +0530

    Add root route for voter page

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.static("public"));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "voter.html"));
+});
 
 // ================= DATABASE CONNECTION =================
 
